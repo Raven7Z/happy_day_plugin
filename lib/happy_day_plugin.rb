6d8ddb14b7e68a6833705122cdbdd46cd4cfc86f
朱佳宝 <zhujiabao3@jd.com>
2021-04-29 15:55:54 +0800

    push

---
+++ lib/happy_day_plugin.rb
@@ -0,0 +1,6 @@
+require "happy_day_plugin/version"
+
+module HappyDayPlugin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/happy_day_plugin/version.rb
@@ -0,0 +1,3 @@
+module HappyDayPlugin
+  VERSION = "0.1.0"
+end
